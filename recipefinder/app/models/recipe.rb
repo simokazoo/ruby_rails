e49c2574d0d97f455adf4546e88b7ed79a081208
simokazoo <french.simona@yahoo.com>
2016-10-13 19:08:39 -0400

    nowww

--- recipefinder/app/models/recipe.rb
+++ recipefinder/app/models/recipe.rb
@@ -1,11 +1,16 @@
-class Recipe
-    include HTTParty
-    ENV["FOOD2FORK_KEY"] = 'd938661a53966dbde85ef0fd5ef26d96'
-  	base_uri 'http://food2fork.com/api'
-  	default_params key: ENV["FOOD2FORK_KEY"]
-  	format :json
 
-  	def self.for term
-    	get("/search", query: { q: term})["recipes"]
-  	end
+require 'httparty'
+
+class Recipe
+  include HTTParty
+
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api"
+  default_params key: key_value
+  format :json
+
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
 end
