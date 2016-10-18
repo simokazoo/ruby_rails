cbef5faa4616113ffbc69d96fcc8888aaeeaf87f
simokazoo <french.simona@yahoo.com>
2016-10-18 12:55:36 -0400

    week1

--- recipefinder/app/models/recipe.rb
+++ recipefinder/app/models/recipe.rb
@@ -4,13 +4,13 @@ require 'httparty'
 class Recipe
   include HTTParty
 
-  key_value = ENV['FOOD2FORK_KEY']
+  ENV['FOOD2FORK_KEY'] = 'd938661a53966dbde85ef0fd5ef26d96'
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api"
-  default_params key: key_value
+  default_params key: ENV['FOOD2FORK_KEY']
   format :json
 
   def self.for (keyword)
-    get("/search", query: {q: keyword})["recipes"]
+    get("", query: {query: keyword})["recipes"]
   end
 end
